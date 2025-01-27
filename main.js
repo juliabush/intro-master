54b899f5eb1f180c2374b91328a5a75d4091552e
juliabush <j.elizabethbush@gmail.com>
2025-01-27 11:24:18 +0100

    Adding JS to project

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 const form = document.getElementById('form');
+const password = document.getElementById('pword');
 
-form.addEventListener('submit', function(e){
-    e.preventDefault
-})
+form.addEventListener('submit', function(e) {
+    let messages = [];
+
+    if (password.value.length > 16) {
+        messages.push('Password cannot be longer than 16 characters');
+    }
+    if (password.value.length < 6) {
+        messages.push('Password must be longer than 6 characters');
+    }
+
+    if (messages.length > 0) {
+        e.preventDefault(); // Prevent form submission
+        alert(messages.join('\n')); // Display error messages
+    }
+});
